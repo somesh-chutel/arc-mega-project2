0eed3b404d70c2912a21c3722e76dcb555547b33
srocknik <somesh.chutel90@gmail.com>
2024-04-03 10:53:42 +0530

    third commit

--- src/Component/Login/index.jsx
+++ src/Component/Login/index.jsx
@@ -1,40 +1,72 @@
+import { useState } from "react";
 import "./index.css";
 
 const Login = () => {
-    const fetchuserDetails = async()=>{
-        const userDetails = {
-            username: "rahu",
-            password: "rahul@2021"
-        }
-        
-        const url = "https://apis.ccbp.in/login";
-        const options = {
-            method: 'POST',
-            body: JSON.stringify(userDetails),
-          }
 
-        const response = await fetch(url,options);
-        const fetchData = await response.json();
-        console.log(fetchData);
+  const [allvalues,setValue] = useState({
+    username:"",
+    password:"",
+    ShowErrorMsg:false,
+    errorMsg:""
+  });
 
+  const onFetchuserDetails = async (event) => {
+    event.preventDefault();
+
+    const userDetails = {
+      username: allvalues.username,
+      password: allvalues.password,
+    };
+
+    const url = "https://apis.ccbp.in/login";
+    const options = {
+      method: "POST",
+      body: JSON.stringify(userDetails),
+    };
+
+    const response = await fetch(url, options);
+    const fetchData = await response.json();
+    console.log(response);
+    console.log(fetchData);
+    if(response.ok===true){
+      setValue({...allvalues,ShowErrorMsg:false})
     }
+    else{
+      setValue({...allvalues,ShowErrorMsg:true,errorMsg:fetchData.error_msg})
+    }
+  };
 
-    fetchuserDetails();
+
+  const onChangeUsername = (event) => {
+    setValue({...allvalues,username:event.target.value});
+
+  };
+
+  const onChangeuserpassword = (event) => {
+    setValue({...allvalues,password:event.target.value});
+  };
+
   return (
     <div className="login-cont">
-      <form className="form-cont">
+      <form className="form-cont" onSubmit={onFetchuserDetails}>
         <div className="img-cont">
-      <img className='web-logo' src="https://assets.ccbp.in/frontend/react-js/logo-img.png" alt="website logo"/>
+          <img
+            className="web-logo"
+            src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+            alt="website logo"
+          />
         </div>
         <div className="mb-3">
           <label htmlFor="exampleInputEmail1" className="form-label">
             Username
           </label>
           <input
-            type="email"
+            type="text"
+            placeholder="rahul"
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            onChange={onChangeUsername}
           />
           <div id="emailHelp" className="form-text">
             We'll never share your email with anyone else.
@@ -46,13 +78,16 @@ const Login = () => {
           </label>
           <input
             type="password"
+            placeholder="rahul@2021"
             className="form-control"
             id="exampleInputPassword1"
+            onChange={onChangeuserpassword}
           />
         </div>
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
+        {allvalues.ShowErrorMsg?(<p>{allvalues.errorMsg}</p>):null}
       </form>
     </div>
   );
