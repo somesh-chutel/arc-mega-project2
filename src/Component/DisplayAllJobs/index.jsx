b2f4a8516e47e789e651a2f8d6fd0ac726f1a864
srocknik <somesh.chutel90@gmail.com>
2024-04-08 14:49:02 +0530

    seventh commit

--- src/Component/DisplayAllJobs/index.jsx
+++ src/Component/DisplayAllJobs/index.jsx
@@ -1,13 +1,58 @@
+import { useEffect, useState } from 'react';
+import Cookies from 'js-cookie'
+import JobsCard from '../jobsCard';
 import './index.css'
 
 
 
 const DisplayAllJobs =()=>{
+    const [allvalues,setValues] = useState({
+        allJobsList:[]
+    });
+
+    useEffect(()=>{
+
+        const fetchAllJobsDetails = async()=>{
+            const token = Cookies.get("jwtToken")
+
+            const url = "https://apis.ccbp.in/jobs";
+            const options = {
+                method:"Get",
+                headers:{
+                    Authorization: `Bearer ${token}`
+                }
+            }
+
+
+            const response = await fetch(url,options);
+            const fetchData = await response.json();
+            console.log(fetchData.jobs[0].title);
+            if(response.ok===true){
+                setValues({...allvalues,allJobsList:fetchData.jobs});
+            }
+
+        }
+
+
+        fetchAllJobsDetails();
+
+
+    },[])
 
 
     return (
 
-        <h1>Display All Jobs Section</h1>
+        <div>
+
+                <ul>
+
+                    {allvalues.allJobsList.map(each=>
+                        <JobsCard jobsDetails={each} key={each.id}/>
+                    )}
+
+                </ul>
+
+        </div>
     )
 }
 
---
+++ src/Component/jobsCard/index.jsx
@@ -0,0 +1,16 @@
+import './index.css'
+
+
+const JobsCard = (props)=>{
+
+    const {jobsDetails} = props;
+
+
+    return (
+        <h1>Jobs Card</h1>
+    )
+}
+
+
+
+export default JobsCard;
